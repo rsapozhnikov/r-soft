4981209fdd42cb62b4d9e3f5abc729e1ca2cf664
roman <catalan@mail.ru>
2015-05-11 12:17:50 +0300

    index

---
+++ index.php
@@ -0,0 +1,59 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("R-soft - web-студия. Сайты на 1С-Битрикс"); ?>
+
+
+
+<div class="index-page">
+
+	<div class="title-button">
+
+		<p class="title">Мы занимаемся профессиональной разработкой web-сайтов на системе 1С-Bitrix</p>
+
+		<button onclick="javascript:location.href='/contacts/'">Связаться с нами</button>
+
+	</div>
+
+	<div class="line">
+		<div class="logoSeparator"></div>
+	</div>
+
+	<div class="index-table">
+
+		<table>
+
+			<tr>
+				<td class="site-work">
+					<a href="/portfolio/"><div class="image"></div></a>
+					<div class="content-block">
+						<div class="block-title"><a href="/portfolio/">Разработка сайтов</a></div>
+						<div class="block-description">Создание сайтов - это наша основная деятельность. Мы занимаемся этим уже более 5 лет. Интернет-магазины, сайты-визитки и прочее.</div>
+					</div>
+				</td>
+				<td class="bitrix">
+					<a href="/bitrix-editions/"><div class="image"></div></a>
+					<div class="content-block">
+						<div class="block-title"><a href="/bitrix-editions/">Лицензии 1C-Bitrix</a></div>
+						<div class="block-description">Покупка лицензий на любые продукты 1С-Bitrix, продление обновлений, покупка дополнительных сайтов, переходы между редакциями.</div>
+					</div>
+				</td>
+
+			</tr>
+
+
+			</tr>
+
+		</table>
+
+	</div>
+
+
+	<div class="line">
+		<div class="logoSeparator"></div>
+	</div>
+
+
+
+</div>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
